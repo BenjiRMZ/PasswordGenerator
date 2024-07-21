70a0c27efa14da862b74f17dc91d356c96fe3012
Beno <160498895+BenjiRMZ@users.noreply.github.com>
2024-07-21 14:55:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-//const _characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-//"/"];
-
+/**
+ * Password Generator
+ * 
+ * This script generates random passwords based on user-selected character types
+ * and length. Users can select whether to include letters, numbers, and symbols.
+ * The generated passwords can be copied to the clipboard with a single click.
+ * @author BenjiRMZ
+ * @version 1.0
+ */
+// Constants for character sets
 const _letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split("");
 const _numbers = "0123456789".split("");
 const _symbols = "~`!@#$%^&*()-_=+{}[]|:;<>.?/".split("");
 
+// Default password length
 let _passwordlength = 12;
+
+// Elements for displaying generated passwords
 let _fieldel1 = document.getElementById("field1");
 let _fieldel2 = document.getElementById("field2");
 
+/*
+* Function to get selected character sets based on user input
+* @return {Array} "characters" - An array of characters based on the selected checkboxes. 
+*/
 function getSelectedCharacters() {
     let characters = [];
     if (document.getElementById("includeLetters").checked) {
@@ -23,6 +37,7 @@ function getSelectedCharacters() {
     return characters;
 }
 
+// Event listener for setting password length
 document.addEventListener("DOMContentLoaded", function() 
 {
     // Add event listeners to each length link
@@ -54,19 +69,32 @@ document.addEventListener("DOMContentLoaded", function()
     
 });
 
+/*
+* Function to set password length based on user selection
+* @param "length" - The desired length of the password.
+*/
 function setPasswordLength(length) 
 {
     _passwordlength = length;
     console.log("Password length set to: " + _passwordlength);
 }
 
-
+/* 
+* Function to generate a random character from the selected character set
+* @param {Array} characters - The array of characters to choose from.
+* @returns {string} A random character from the selected character set.
+*/
 function generateRandomCharacter(characters)
 {
        let randomIndex = Math.floor(Math.random()* characters.length);
        return characters[randomIndex];
 }
 
+/*
+* Function to generate passwords and display them in the result fields
+* @return _field1 generated password1
+* @return _field2 generated password2
+*/
 function generatePassword()
 {
     let characters = getSelectedCharacters();
@@ -90,6 +118,11 @@ function generatePassword()
    return _fieldel1 && _fieldel2;
 }
 
+/*
+* Function to copy password to clipboard
+* @param {string} passwordText - The text of the password to be copied.
+* @param {HTML-Element} copyButton - The button element that triggers the copy action.
+*/
 function copyPassword(passwordtext,copybutton)
 {
       navigator.clipboard.writeText(passwordtext).then(function()
@@ -103,6 +136,7 @@ function copyPassword(passwordtext,copybutton)
     });
 }
 
+// Event listener for copying the generated password into your clipboard
 _fieldel1.addEventListener("click", function()
 {
     copyPassword(_fieldel1.textContent, _fieldel1)
